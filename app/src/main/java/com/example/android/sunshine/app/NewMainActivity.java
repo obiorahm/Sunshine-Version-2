3d081d880cb76bbe8eb7359ea3962e4c59f5fc45
Mmachi Obiorah <obiorahm@gmail.com>
2017-04-18 13:05:07 -0500

    new main with an option to go to the gallery and one to capture an image

---
+++ app/src/main/java/com/example/android/sunshine/app/NewMainActivity.java
@@ -0,0 +1,55 @@
+package com.example.android.sunshine.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+/**
+ * Created by mgo983 on 4/18/17.
+ */
+
+public class NewMainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new SelectActionFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/android/sunshine/app/SelectActionFragment.java
@@ -0,0 +1,63 @@
+package com.example.android.sunshine.app;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+
+/**
+ * Created by mgo983 on 4/18/17.
+ */
+
+public class SelectActionFragment extends android.support.v4.app.Fragment {
+
+    public final static String EXTRA_TEXT = "com.example.android.sunshine.MESSAGE";
+
+    public SelectActionFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.newmain, container, false);
+
+
+//click listener for image buttons
+
+        ImageButton cameraButton = (ImageButton) rootView.findViewById(R.id.camera);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent cameraActivityIntent = new Intent(getActivity(), CameraActivity.class);
+                startActivity(cameraActivityIntent);
+            }
+        });
+
+
+        return rootView;
+
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu (Menu menu, MenuInflater inflater){
+        inflater.inflate(R.menu.forecastfragment,menu);
+    }
+
+}
